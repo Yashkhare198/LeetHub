33ccc97f8767111e5422f7e32be2af01c2c5479f
Yashkhare198 <64392890+Yashkhare198@users.noreply.github.com>
2023-07-02 20:46:16 +0530

    Added solution - LeetHub

---
+++ Copy Set Bits in Range - GFG/copy-set-bits-in-range.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution{
+    public:
+    int setSetBit(int x, int y, int l, int r){
+        
+        for(int i=l;i<=r;i++){
+            
+            int a = (1<<(i-1))&y;
+            
+            x =x|a;
+            
+        }
+        return x;
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y, l, r;
+        cin>>x>>y>>l>>r;
+        
+        Solution ob;
+        cout<<ob.setSetBit(x, y, l, r)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
